78d8c6561b9121b5a4ba50a470a4296b78ac6372
ghabrielferrari <ghcferrari75@gmail.com>
2024-10-03 20:00:37 -0300

    Added Firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,6 +40,10 @@ android {
 }
 
 dependencies {
+
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
