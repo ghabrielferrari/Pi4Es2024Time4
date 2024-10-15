b6fce835cbb5c80a3b1cad074f1b4a7b915fc2a4
Gabriel Ferrari <ghcferrari75@gmail.com>
2024-10-15 17:04:14 -0300

    ProfileSettingsFragment Upgraded

--- app/src/main/java/br/edu/puccampinas/pi4es2024time4/model/ProfileSettingsViewModel.kt
+++ app/src/main/java/br/edu/puccampinas/pi4es2024time4/model/ProfileSettingsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProfileSettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Profile Settings home Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
